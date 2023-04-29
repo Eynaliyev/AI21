dcded91e1f14949b444d6662716c8dda2a8288de
Eynaliyev <rustam.eynaliyev@gmail.com>
2023-04-29 15:37:25 +0200

    add: AI21 lab's jurassic implementation

--- src/models.py
+++ src/models.py
@@ -1,19 +1,19 @@
 import os
 from langchain.llms import AI21
-# from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv
 # from langchain.chat_models import ChatOpenAI
 
-# _ = load_dotenv(find_dotenv())
+_ = load_dotenv(find_dotenv())
 # openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
 def create_models():
     return [
-        # {
-        #     "model_name": 'j2-jumbo-instruct',
-        #     "model_provider": "AI21",
-        #     "model": AI21(model="j2-jumbo-instruct")
-        # },
+        {
+            "model_name": 'j2-jumbo-instruct',
+            "model_provider": "AI21",
+            "model": AI21(model="j2-jumbo-instruct")
+        },
         {
             "model_name": 'gpt-3.5-turbo',
             "model_provider": "OpenAI",
